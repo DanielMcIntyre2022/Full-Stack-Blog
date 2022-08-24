d2d04ff9254a7e6772ef08b544986d0d71e5ae0d
Daniel <daniel-mcintyre@hotmail.com>
2022-08-24 11:57:35 -0400

    created form for adding articles

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
         createdAt: new Date(),
         description: 'Test descripton'
     }]
-    res.render('index', {articles: articles})
+    res.render('articles/index', {articles: articles})
 });
 
 app.listen(4000);
