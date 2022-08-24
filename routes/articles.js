a1a7d009b6f428de629d098688c65cc9205b7ff4
Daniel <daniel-mcintyre@hotmail.com>
2022-08-24 13:32:02 -0400

    implemented logic for article posting

--- routes/articles.js
+++ routes/articles.js
@@ -6,8 +6,22 @@ router.get('/new', (req, res) => {
     res.render('articles/new');
 });
 
-router.post('/', (req, res) => {
+router.get('/:id', (res, req) => {
 
+})
+
+router.post('/', async (req, res) => {
+    const article = new Article({
+        title: req.body.title,
+        description: req.body.descriptions,
+        markdown: req.body.markdown
+    })
+    try {
+        article = await article.save()
+        res.redirect(`/articles/${article.id}`)
+    } catch (e) {
+        res.render('articles/new', { article: article })
+    }
 })
 
 module.exports = router;
